45f4e557dddff315efcf041919750e478d8c78f6
Maurício Mafra Monnerat <monnerat.mauricio@gmail.com>
2025-10-09 18:07:50 -0300

    feat: refactoring main

--- src/modules/scripts/baseScreen.lua
+++ src/modules/scripts/baseScreen.lua
@@ -4,9 +4,9 @@ function nebula.setup()
     Sprite = nebula.ecs.component("Sprite")
     Position = nebula.ecs.component("Position")
     Quad = nebula.ecs.component("Quad")
-    boxSprite = nebula.graphics.newSprite("resources/textures/container.jpg")
+    --boxSprite = nebula.graphics.newSprite("resources/textures/container.jpg")
     player = nebula.ecs.spawn()
-    nebula.ecs.addComponent(player, Position({x = 10; y = 10}), Sprite({texture = boxSprite}))
+    nebula.ecs.addComponent(player, Position({x = 10; y = 10}), Quad({height = 200; width = 300}))
 end
 
 function nebula.update(dt)
--- src/nebula.cpp
+++ src/nebula.cpp
@@ -32,21 +32,6 @@ static inline bool endsWith(const std::string &full, const std::string &ending) 
     return full.compare(full.size() - ending.size(), ending.size(), ending) == 0;
 }
 
-//static inline bool removeSubstr(std::string &full, const std::string &substring) {
-//    if (substring.size() > full.size()) {
-//        return false;
-//    }
-//
-//    size_t pos = full.find(substring);
-//
-//    if (pos == std::string::npos) {
-//        return false;
-//    }
-//
-//    full.erase(pos, substring.length());
-//    return true;
-//}
-
 
 static RunAction runNebula(int argc, char **argv, int &mainReturn) {
     lua_State *L = luaL_newstate();
@@ -134,3 +119,61 @@ int main(int argc, char **argv) {
     SDL_Quit();
     return mainReturn;
 }
+
+//int main2() {
+//    bool run = true;
+//
+//    nebula::setup();
+//
+//    nebula::graphics::Graphics *graphics = nebula::ModuleRegistry::getInstance<nebula::graphics::Graphics>(nebula::GRAPHICS);
+//    nebula::ecs::World *ecs = nebula::ModuleRegistry::getInstance<nebula::ecs::World>(nebula::ECS);
+//    nebula::window::Window *window = nebula::ModuleRegistry::getInstance<nebula::window::Window>(nebula::WINDOW);
+//    nebula::time::Time *time = nebula::ModuleRegistry::getInstance<nebula::time::Time>(nebula::TIME);
+//
+//    auto ent1 = ecs->spawn();
+//    auto ent2 = ecs->spawn();
+//
+//    auto ninaSprite = graphics->newSprite("resources/textures/Nina.png");
+//
+//    ecs->addComponent(ent1, Sprite{ninaSprite});
+//    ecs->addComponent(ent1, Position{10.0f, 10.0f});
+//    ecs->addComponent(ent1, Scale{0.5f, 0.5f});
+//
+//    ecs->addComponent(ent2, Quad{100.0f, 100.0f});
+//    ecs->addComponent(ent2, Position{500.0f, 200.0f});
+//
+//    graphics->setBackground(1.0f, 0.0f, 0.0f);
+//
+//    time->tick();
+//
+//    while (run) {
+//        SDL_Event event{0};
+//        while(SDL_PollEvent(&event)) {
+//            switch (event.type) {
+//                case SDL_EVENT_QUIT: {
+//                    run = false;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        auto dt = time->tick();
+//
+//        Position *e1pos = ecs->getComponent<Position>(ent1);
+//        e1pos->x += 10.0f * dt;
+//
+//        Position *e2pos = ecs->getComponent<Position>(ent2);
+//        e2pos->x -= 2.0f * dt;
+//        e2pos->y -= 1.0f * dt;
+//
+//        graphics->beginScene(ecs);
+//        graphics->draw(ent1);
+//        graphics->draw(ent2);
+//        graphics->endScene();
+//        window->swapBuffers();
+//    }
+//
+//    SDL_Quit();
+//
+//    return 0;
+//}
